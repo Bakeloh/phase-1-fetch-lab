6a4607c34ef9b48ab9d517e9e7739a5b8bda41b0
Bakeloh <bakelohnoel@gmail.com>
2023-11-02 08:13:57 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((json) => console.log(json))
+    .catch((error) => console.error("Error fetching books:", error));
+  /*if books cant be fetteched*/
+}
+
+function fetchBooks() {
+  // Fetch the data and return the fetch promise.
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((books) => renderBooks(books))
+    .catch((error) => console.error("Error fetching books:", error));
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  main.innerHTML = ''; /*Clear the main element before rendering new books.*/
+  books.forEach((book) => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.textContent = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+document.addEventListener('DOMContentLoaded', function () {
+  fetchBooks().then(() => {
+    console.log("Books fetched and rendered.");
+  });
 });
+
+////////////////////////////////////////////////////////////////////
+
